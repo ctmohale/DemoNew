3899d0867147c9f2ba2a0ac7074fffa493b34364
Chris <mohalebrown@gmail.com>
2024-12-12 11:32:27 +0200

    Some comment

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,16 @@ require __DIR__.'/auth.php';
 
 //Add new test route
 require __DIR__.'/auth.php';
+
+
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+
